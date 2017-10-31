31ca1415e0a1bc2185d0d7b470c4216a2657c34f
Arman Ashrafian <ashy63@gmail.com>
2017-10-30 23:42:18 -0700

    messin with net/http and templates

---
+++ wiki.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"html/template"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+// Save Page's body to text file
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+// Load text file by title
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{title, body}, nil
+}
+
+// Route: "/"
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, `<h1>FUCKYA BITCHES GOLANG FOR THE WIN</h1>
+					<h3>%s</h3>`, r.URL.Path[1:])
+}
+
+// Route: "/view"
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, err := loadPage(title)
+
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+
+	t, _ := template.ParseFiles("view.html")
+	t.Execute(w, p)
+}
+
+// Route: "/edit"
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, err := loadPage(title)
+
+	if err != nil {
+		p = &Page{Title: title}
+	}
+
+	t, _ := template.ParseFiles("edit.html")
+	t.Execute(w, p)
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	// http.HandleFunc("/save/", saveHandler)
+
+	fmt.Println("SERVING ON PORT 8080")
+	http.ListenAndServe(":8080", nil)
+}
